6f41b5c8e4bb92737bab93a1d46377565e02259f
majharul <majharuljony163@gmail.com>
2022-06-04 16:28:01 +0600

    Learn redux logger

--- index.js
+++ index.js
@@ -8,24 +8,18 @@
 
 */
 
-const {createStore,combineReducers}=require("redux");
+const {createStore,applyMiddleware}=require("redux");
+const {logger} = require("redux-logger");
 
 //define constant
 const GET_PRODUCTS="GET_PRODUCTS";
 const ADD_PRODUCTS="ADD_PRODUCTS";
-
-const GET_CART="GET_CART";
-const ADD_TO_CART="ADD_TO_CART";
 
 //define initial state
 const initialProductsState={
     products:["sugar","salt"],
     totalProducts:2
 };
-const initialCartState={
-    cart:["am","kola"],
-    cartQuantity:2
-}
 
 //define action
 const getProducts=()=>{
@@ -36,18 +30,6 @@ const getProducts=()=>{
 const addProduct=(product)=>{
     return{
         type:ADD_PRODUCTS,
-        payload:product
-    }
-};
-
-const getCart=()=>{
-    return{
-        type:GET_CART,
-    }
-};
-const addToCart=(product)=>{
-    return{
-        type:ADD_TO_CART,
         payload:product
     }
 };
@@ -70,38 +52,16 @@ const productReducer= (state=initialProductsState,action) => {
   }
 };
 
-const cartReducer= (state=initialCartState,action) => {
-  switch (action.type) {
-      case GET_CART:
-          return{
-              ...state
-          };
-      case ADD_TO_CART:
-          return {
-              cart: [...state.cart,action.payload],
-              cartQuantity: state.cartQuantity + 1
-          };
-      default:
-         return  state;
-  }
-};
-
-//combine multiple reducer
-const rootReducer =combineReducers({
-    products:productReducer,
-    cart:cartReducer
-})
 
 //store
-const store = createStore(rootReducer);
+const store = createStore(productReducer,applyMiddleware(logger));
 store.subscribe(()=>{
     console.log(store.getState());
 });
 
 //action dispatch
-// store.dispatch(addProduct("oil"));
+store.dispatch(addProduct("oil"));
 store.dispatch(getProducts());
-// store.dispatch(addToCart("oil"));
-store.dispatch(getCart());
+
 
 
